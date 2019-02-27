9a314a39559d5bff11f4a6929e39f1c29aa63494
johara guadalupe jimenez acevedo <johara_jimenez@hotmail.com>
2019-02-27 14:27:18 -0700

    borr

--- WindowsFormsApp1/WindowsFormsApp1/Views/ViewTabs.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Views/ViewTabs.Designer.cs
@@ -190,18 +190,18 @@
             // 
             // dataGridView1
             // 
-            //this.dataGridView1.AllowUserToOrderColumns = true;
-            //this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            //this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            //this.ID,
-            //this.Descripcion,
-            //this.Estado,
-            //this.idResponsable});
-            //this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            //this.dataGridView1.Location = new System.Drawing.Point(3, 3);
-            //this.dataGridView1.Name = "dataGridView1";
-            //this.dataGridView1.Size = new System.Drawing.Size(783, 418);
-            //this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Descripcion,
+            this.Estado,
+            this.idResponsable});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(783, 418);
+            this.dataGridView1.TabIndex = 0;
             //// 
             // ID
             // 
