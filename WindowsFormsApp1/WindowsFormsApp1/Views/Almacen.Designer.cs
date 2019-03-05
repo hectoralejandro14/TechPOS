16931450a64bce3cbd20c02ab492ea4e90ffcc11
Lizbeth Martínez Terán <eryklmtz@hotmail.com>
2019-03-05 15:05:07 -0700

    Nombres

--- WindowsFormsApp1/WindowsFormsApp1/Views/Almacen.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Views/Almacen.Designer.cs
@@ -28,39 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.EqAlmacenLbl = new System.Windows.Forms.Label();
+            this.almacenTbl = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.almacenTbl)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // EqAlmacenLbl
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(201, 47);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(202, 23);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Equipos de Almacén";
+            this.EqAlmacenLbl.AutoSize = true;
+            this.EqAlmacenLbl.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EqAlmacenLbl.Location = new System.Drawing.Point(201, 47);
+            this.EqAlmacenLbl.Name = "EqAlmacenLbl";
+            this.EqAlmacenLbl.Size = new System.Drawing.Size(202, 23);
+            this.EqAlmacenLbl.TabIndex = 0;
+            this.EqAlmacenLbl.Text = "Equipos de Almacén";
             // 
-            // dataGridView1
+            // almacenTbl
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.almacenTbl.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.almacenTbl.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2,
             this.Column3,
             this.Column5,
             this.Column4});
-            this.dataGridView1.Location = new System.Drawing.Point(21, 113);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(583, 235);
-            this.dataGridView1.TabIndex = 1;
+            this.almacenTbl.Location = new System.Drawing.Point(21, 113);
+            this.almacenTbl.Name = "almacenTbl";
+            this.almacenTbl.Size = new System.Drawing.Size(583, 235);
+            this.almacenTbl.TabIndex = 1;
             // 
             // Column1
             // 
@@ -93,12 +93,12 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(616, 450);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.almacenTbl);
+            this.Controls.Add(this.EqAlmacenLbl);
             this.Name = "Almacen";
             this.Text = "Almacen";
             this.Load += new System.EventHandler(this.Almacen_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.almacenTbl)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,8 +106,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label EqAlmacenLbl;
+        private System.Windows.Forms.DataGridView almacenTbl;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
--- WindowsFormsApp1/WindowsFormsApp1/Views/Cobrar.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Views/Cobrar.Designer.cs
@@ -28,121 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.totalLbl = new System.Windows.Forms.Label();
+            this.tipoPagoLlb = new System.Windows.Forms.Label();
+            this.cambioLbl = new System.Windows.Forms.Label();
+            this.efectivoLbl = new System.Windows.Forms.Label();
+            this.totalFinalLbl = new System.Windows.Forms.Label();
+            this.cancelarBtnCobrar = new System.Windows.Forms.Button();
+            this.cobrarBtn = new System.Windows.Forms.Button();
+            this.cobrarLbl = new System.Windows.Forms.Label();
+            this.tipoPagoCbx = new System.Windows.Forms.ComboBox();
+            this.efectivoTbx = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // label1
+            // totalLbl
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(83, 76);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Total:";
+            this.totalLbl.AutoSize = true;
+            this.totalLbl.Location = new System.Drawing.Point(83, 76);
+            this.totalLbl.Name = "totalLbl";
+            this.totalLbl.Size = new System.Drawing.Size(34, 13);
+            this.totalLbl.TabIndex = 0;
+            this.totalLbl.Text = "Total:";
             // 
-            // label2
+            // tipoPagoLlb
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(83, 115);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Tipo de pago:";
+            this.tipoPagoLlb.AutoSize = true;
+            this.tipoPagoLlb.Location = new System.Drawing.Point(83, 115);
+            this.tipoPagoLlb.Name = "tipoPagoLlb";
+            this.tipoPagoLlb.Size = new System.Drawing.Size(73, 13);
+            this.tipoPagoLlb.TabIndex = 1;
+            this.tipoPagoLlb.Text = "Tipo de pago:";
             // 
-            // label3
+            // cambioLbl
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(87, 200);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Cambio:";
+            this.cambioLbl.AutoSize = true;
+            this.cambioLbl.Location = new System.Drawing.Point(87, 200);
+            this.cambioLbl.Name = "cambioLbl";
+            this.cambioLbl.Size = new System.Drawing.Size(45, 13);
+            this.cambioLbl.TabIndex = 2;
+            this.cambioLbl.Text = "Cambio:";
             // 
-            // label4
+            // efectivoLbl
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(87, 158);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Efectivo:";
+            this.efectivoLbl.AutoSize = true;
+            this.efectivoLbl.Location = new System.Drawing.Point(87, 158);
+            this.efectivoLbl.Name = "efectivoLbl";
+            this.efectivoLbl.Size = new System.Drawing.Size(49, 13);
+            this.efectivoLbl.TabIndex = 3;
+            this.efectivoLbl.Text = "Efectivo:";
             // 
-            // label5
+            // totalFinalLbl
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(123, 76);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(13, 13);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "$";
+            this.totalFinalLbl.AutoSize = true;
+            this.totalFinalLbl.Location = new System.Drawing.Point(123, 76);
+            this.totalFinalLbl.Name = "totalFinalLbl";
+            this.totalFinalLbl.Size = new System.Drawing.Size(13, 13);
+            this.totalFinalLbl.TabIndex = 4;
+            this.totalFinalLbl.Text = "$";
             // 
-            // button1
+            // cancelarBtnCobrar
             // 
-            this.button1.Location = new System.Drawing.Point(114, 262);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Cancelar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.cancelarBtnCobrar.Location = new System.Drawing.Point(114, 262);
+            this.cancelarBtnCobrar.Name = "cancelarBtnCobrar";
+            this.cancelarBtnCobrar.Size = new System.Drawing.Size(75, 23);
+            this.cancelarBtnCobrar.TabIndex = 5;
+            this.cancelarBtnCobrar.Text = "Cancelar";
+            this.cancelarBtnCobrar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // cobrarBtn
             // 
-            this.button2.Location = new System.Drawing.Point(254, 262);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Cobrar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.cobrarBtn.Location = new System.Drawing.Point(254, 262);
+            this.cobrarBtn.Name = "cobrarBtn";
+            this.cobrarBtn.Size = new System.Drawing.Size(75, 23);
+            this.cobrarBtn.TabIndex = 6;
+            this.cobrarBtn.Text = "Cobrar";
+            this.cobrarBtn.UseVisualStyleBackColor = true;
             // 
-            // label6
+            // cobrarLbl
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(156, 9);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(97, 31);
-            this.label6.TabIndex = 7;
-            this.label6.Text = "Cobrar";
+            this.cobrarLbl.AutoSize = true;
+            this.cobrarLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cobrarLbl.Location = new System.Drawing.Point(156, 19);
+            this.cobrarLbl.Name = "cobrarLbl";
+            this.cobrarLbl.Size = new System.Drawing.Size(97, 31);
+            this.cobrarLbl.TabIndex = 7;
+            this.cobrarLbl.Text = "Cobrar";
             // 
-            // comboBox1
+            // tipoPagoCbx
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(162, 112);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(97, 21);
-            this.comboBox1.TabIndex = 8;
+            this.tipoPagoCbx.FormattingEnabled = true;
+            this.tipoPagoCbx.Location = new System.Drawing.Point(162, 112);
+            this.tipoPagoCbx.Name = "tipoPagoCbx";
+            this.tipoPagoCbx.Size = new System.Drawing.Size(97, 21);
+            this.tipoPagoCbx.TabIndex = 8;
             // 
-            // textBox1
+            // efectivoTbx
             // 
-            this.textBox1.Location = new System.Drawing.Point(162, 151);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 9;
+            this.efectivoTbx.Location = new System.Drawing.Point(162, 151);
+            this.efectivoTbx.Name = "efectivoTbx";
+            this.efectivoTbx.Size = new System.Drawing.Size(100, 20);
+            this.efectivoTbx.TabIndex = 9;
             // 
             // Cobrar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(400, 306);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.efectivoTbx);
+            this.Controls.Add(this.tipoPagoCbx);
+            this.Controls.Add(this.cobrarLbl);
+            this.Controls.Add(this.cobrarBtn);
+            this.Controls.Add(this.cancelarBtnCobrar);
+            this.Controls.Add(this.totalFinalLbl);
+            this.Controls.Add(this.efectivoLbl);
+            this.Controls.Add(this.cambioLbl);
+            this.Controls.Add(this.tipoPagoLlb);
+            this.Controls.Add(this.totalLbl);
             this.Name = "Cobrar";
             this.Text = "Cobrar";
             this.ResumeLayout(false);
@@ -152,15 +152,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label totalLbl;
+        private System.Windows.Forms.Label tipoPagoLlb;
+        private System.Windows.Forms.Label cambioLbl;
+        private System.Windows.Forms.Label efectivoLbl;
+        private System.Windows.Forms.Label totalFinalLbl;
+        private System.Windows.Forms.Button cancelarBtnCobrar;
+        private System.Windows.Forms.Button cobrarBtn;
+        private System.Windows.Forms.Label cobrarLbl;
+        private System.Windows.Forms.ComboBox tipoPagoCbx;
+        private System.Windows.Forms.TextBox efectivoTbx;
     }
 }
