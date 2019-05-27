c2709a33992ad8c3c07c754625e6ecfe6496fcd3
Sergio FloreS <jefemaestroop7@hotmail.com>
2019-05-27 07:58:54 -0700

    Cancelacion de producto (incompleto)

---
+++ WindowsFormsApp1/WindowsFormsApp1/Views/CancelarProductoVenta.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace WindowsFormsApp1.Views
+{
+    partial class CancelarProductoVenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCodigoCancelacion = new System.Windows.Forms.TextBox();
+            this.txtCantidadCancelacion = new System.Windows.Forms.TextBox();
+            this.txtDescripcionCancelacion = new System.Windows.Forms.TextBox();
+            this.txtTotalDeCancelacion = new System.Windows.Forms.TextBox();
+            this.lblCodigoS = new System.Windows.Forms.Label();
+            this.lblDescripcionSC = new System.Windows.Forms.Label();
+            this.lblTotalSC = new System.Windows.Forms.Label();
+            this.lblCantidadSC = new System.Windows.Forms.Label();
+            this.lblCancelarProductoS = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMotivoDeCancelacion = new System.Windows.Forms.TextBox();
+            this.SpanelLogo = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnAceptarCancelacionVenta = new System.Windows.Forms.Button();
+            this.SpanelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCodigoCancelacion
+            // 
+            this.txtCodigoCancelacion.Location = new System.Drawing.Point(358, 66);
+            this.txtCodigoCancelacion.Name = "txtCodigoCancelacion";
+            this.txtCodigoCancelacion.Size = new System.Drawing.Size(382, 20);
+            this.txtCodigoCancelacion.TabIndex = 0;
+            // 
+            // txtCantidadCancelacion
+            // 
+            this.txtCantidadCancelacion.Location = new System.Drawing.Point(358, 157);
+            this.txtCantidadCancelacion.Name = "txtCantidadCancelacion";
+            this.txtCantidadCancelacion.Size = new System.Drawing.Size(382, 20);
+            this.txtCantidadCancelacion.TabIndex = 1;
+            // 
+            // txtDescripcionCancelacion
+            // 
+            this.txtDescripcionCancelacion.Location = new System.Drawing.Point(358, 116);
+            this.txtDescripcionCancelacion.Name = "txtDescripcionCancelacion";
+            this.txtDescripcionCancelacion.Size = new System.Drawing.Size(382, 20);
+            this.txtDescripcionCancelacion.TabIndex = 3;
+            // 
+            // txtTotalDeCancelacion
+            // 
+            this.txtTotalDeCancelacion.Location = new System.Drawing.Point(358, 200);
+            this.txtTotalDeCancelacion.Name = "txtTotalDeCancelacion";
+            this.txtTotalDeCancelacion.Size = new System.Drawing.Size(382, 20);
+            this.txtTotalDeCancelacion.TabIndex = 4;
+            // 
+            // lblCodigoS
+            // 
+            this.lblCodigoS.AutoSize = true;
+            this.lblCodigoS.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigoS.Location = new System.Drawing.Point(271, 68);
+            this.lblCodigoS.Name = "lblCodigoS";
+            this.lblCodigoS.Size = new System.Drawing.Size(72, 18);
+            this.lblCodigoS.TabIndex = 5;
+            this.lblCodigoS.Text = "Código : ";
+            // 
+            // lblDescripcionSC
+            // 
+            this.lblDescripcionSC.AutoSize = true;
+            this.lblDescripcionSC.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcionSC.Location = new System.Drawing.Point(241, 115);
+            this.lblDescripcionSC.Name = "lblDescripcionSC";
+            this.lblDescripcionSC.Size = new System.Drawing.Size(100, 18);
+            this.lblDescripcionSC.TabIndex = 11;
+            this.lblDescripcionSC.Text = "Descripcion :";
+            // 
+            // lblTotalSC
+            // 
+            this.lblTotalSC.AutoSize = true;
+            this.lblTotalSC.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalSC.Location = new System.Drawing.Point(288, 199);
+            this.lblTotalSC.Name = "lblTotalSC";
+            this.lblTotalSC.Size = new System.Drawing.Size(48, 18);
+            this.lblTotalSC.TabIndex = 12;
+            this.lblTotalSC.Text = "Total :";
+            // 
+            // lblCantidadSC
+            // 
+            this.lblCantidadSC.AutoSize = true;
+            this.lblCantidadSC.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidadSC.Location = new System.Drawing.Point(257, 156);
+            this.lblCantidadSC.Name = "lblCantidadSC";
+            this.lblCantidadSC.Size = new System.Drawing.Size(84, 18);
+            this.lblCantidadSC.TabIndex = 14;
+            this.lblCantidadSC.Text = "Cantidad : ";
+            // 
+            // lblCancelarProductoS
+            // 
+            this.lblCancelarProductoS.AutoSize = true;
+            this.lblCancelarProductoS.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCancelarProductoS.Location = new System.Drawing.Point(365, 12);
+            this.lblCancelarProductoS.Name = "lblCancelarProductoS";
+            this.lblCancelarProductoS.Size = new System.Drawing.Size(256, 33);
+            this.lblCancelarProductoS.TabIndex = 16;
+            this.lblCancelarProductoS.Text = "Cancelar Producto";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(282, 246);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 18);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Motivo";
+            // 
+            // txtMotivoDeCancelacion
+            // 
+            this.txtMotivoDeCancelacion.Location = new System.Drawing.Point(358, 247);
+            this.txtMotivoDeCancelacion.Multiline = true;
+            this.txtMotivoDeCancelacion.Name = "txtMotivoDeCancelacion";
+            this.txtMotivoDeCancelacion.Size = new System.Drawing.Size(382, 119);
+            this.txtMotivoDeCancelacion.TabIndex = 18;
+            // 
+            // SpanelLogo
+            // 
+            this.SpanelLogo.BackColor = System.Drawing.Color.DimGray;
+            this.SpanelLogo.Controls.Add(this.pictureBox1);
+            this.SpanelLogo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.SpanelLogo.Location = new System.Drawing.Point(0, 0);
+            this.SpanelLogo.Name = "SpanelLogo";
+            this.SpanelLogo.Size = new System.Drawing.Size(175, 466);
+            this.SpanelLogo.TabIndex = 20;
+            this.SpanelLogo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.SpanelLogo_MouseDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.logoUp;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(163, 231);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnAceptarCancelacionVenta
+            // 
+            this.btnAceptarCancelacionVenta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnAceptarCancelacionVenta.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAceptarCancelacionVenta.FlatAppearance.BorderSize = 0;
+            this.btnAceptarCancelacionVenta.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnAceptarCancelacionVenta.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnAceptarCancelacionVenta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptarCancelacionVenta.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptarCancelacionVenta.ForeColor = System.Drawing.Color.Orange;
+            this.btnAceptarCancelacionVenta.Location = new System.Drawing.Point(358, 382);
+            this.btnAceptarCancelacionVenta.Name = "btnAceptarCancelacionVenta";
+            this.btnAceptarCancelacionVenta.Size = new System.Drawing.Size(382, 35);
+            this.btnAceptarCancelacionVenta.TabIndex = 12;
+            this.btnAceptarCancelacionVenta.Text = "Acepatar";
+            this.btnAceptarCancelacionVenta.UseVisualStyleBackColor = false;
+            this.btnAceptarCancelacionVenta.Click += new System.EventHandler(this.btnAceptarCancelacionVenta_Click);
+            // 
+            // CancelarProductoVenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 466);
+            this.Controls.Add(this.btnAceptarCancelacionVenta);
+            this.Controls.Add(this.SpanelLogo);
+            this.Controls.Add(this.txtMotivoDeCancelacion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCancelarProductoS);
+            this.Controls.Add(this.lblCantidadSC);
+            this.Controls.Add(this.lblTotalSC);
+            this.Controls.Add(this.lblDescripcionSC);
+            this.Controls.Add(this.lblCodigoS);
+            this.Controls.Add(this.txtTotalDeCancelacion);
+            this.Controls.Add(this.txtDescripcionCancelacion);
+            this.Controls.Add(this.txtCantidadCancelacion);
+            this.Controls.Add(this.txtCodigoCancelacion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CancelarProductoVenta";
+            this.Text = "CancelarProductoVenta";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.CancelarProductoVenta_MouseDown);
+            this.SpanelLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCodigoCancelacion;
+        private System.Windows.Forms.TextBox txtCantidadCancelacion;
+        private System.Windows.Forms.TextBox txtDescripcionCancelacion;
+        private System.Windows.Forms.TextBox txtTotalDeCancelacion;
+        private System.Windows.Forms.Label lblCodigoS;
+        private System.Windows.Forms.Label lblDescripcionSC;
+        private System.Windows.Forms.Label lblTotalSC;
+        private System.Windows.Forms.Label lblCantidadSC;
+        private System.Windows.Forms.Label lblCancelarProductoS;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMotivoDeCancelacion;
+        private System.Windows.Forms.Panel SpanelLogo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnAceptarCancelacionVenta;
+    }
+}
